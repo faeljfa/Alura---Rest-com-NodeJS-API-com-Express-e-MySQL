b02d4e3ebd28e52042dbb3e868ba341f29cb7c84
rafaelo_001 <rafaelo_001@cercred.local>
2021-09-14 16:33:25 -0300

    Criação de rotas para o atendimento

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,8 @@
 //exportando o módulo para que possa ser utilizado em nossa aplicação.
 module.exports = app => {
-    //Rota para a página de atendimentos
-    app.get('/atendimentos', (req, res) => {res.send('Rota de atendimentos')})
+    //Rota para a página de atendimentos via metodo get
+    app.get('/atendimentos', (req, res) => {res.send('Rota de atendimentos via get')})
+
+    //Rota para a página de atendimentos via metodo post
+    app.post('/atendimentos', (req, res) => {res.send("Voce está na rota de atendimentos e utilizando post")})
 }
